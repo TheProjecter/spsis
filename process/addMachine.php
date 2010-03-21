43cce8d895a868d1043f1cca55e6a3b1ae4e84c7
flapamittan <flapamittan@4ce4fa8e-10c2-11df-8156-69f583257f85>
2010-03-21 16:03:12 +0000

    Updated process/addMachine.php message display

--- process/addMachine.php
+++ process/addMachine.php
@@ -3,6 +3,9 @@
 	include '../ajax/sessions.inc';
 	$result = mysql_query("INSERT INTO machine VALUES(NULL, '$_GET[mach]')");
 	$ref = $_SERVER['HTTP_REFERER'];
-	header( 'refresh: 0; url='.$ref);
+	//header( 'refresh: 0; url='.$ref);
+	if($result){
+		echo "Successfully Added";
+	}
 
 ?>
