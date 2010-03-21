f623f8dd18dcc7c40d8b5b2dbbc91e315c43cc11
flapamittan <flapamittan@4ce4fa8e-10c2-11df-8156-69f583257f85>
2010-03-21 16:04:03 +0000

    Updated process/addItem.php message display

--- trunk/process/addItem.php
+++ trunk/process/addItem.php
@@ -4,11 +4,13 @@
 	
 	$query = "INSERT INTO item VALUES(NULL, '$_GET[mat_no]', '$_GET[desc1]', '$_GET[stock]', '$_GET[bin]', '$_GET[bun]', '$_GET[cc]', '$_GET[itemType]', '$_GET[machine]')";
 	$result = mysql_query($query);
-	
+	echo "Successfully Added";
+	/*
 	if($_SESSION['type']=='admin'){
 		echo "<script>document.location='../mainForAdmin.php'</script>";
 	}
 	else{
 		echo "<script>document.location='../mainForRegUser.php'</script>";
 	}
+	*/
 ?>
