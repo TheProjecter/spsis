cb39dbd168caa7b863f69c3e51b2928705a6ad7b
fojr711 <fojr711@4ce4fa8e-10c2-11df-8156-69f583257f85>
2010-03-21 15:09:35 +0000

    input checking

--- trunk/ajax/editItem.php
+++ trunk/ajax/editItem.php
@@ -47,11 +47,11 @@
 		}
 	}
 	echo "<tr><td class='ui-widget-header '>Material No.</td><td>" . $rows['matno'] . "</td></tr>";
-	echo "<tr><td class='ui-widget-header '>Description</td><td><input type='text' name='item_desc1' id='desc1_item' value='" . $rows['desc1'] . "'></td></tr>";
+	echo "<tr><td class='ui-widget-header '>Description</td><td><input type='text' maxlength='50' name='item_desc1' id='desc1_item' value='" . $rows['desc1'] . "' class='required'></td></tr>";
 	echo "<tr><td class='ui-widget-header '>Stock</td><td>" . $rows['stock'] . "</td></tr>";
-	echo "<tr><td class='ui-widget-header '>Bin</td><td><input type='text' name='item_bin' id='bin_item' value='" . $rows['bin'] . "'></td></tr>";
-	echo "<tr><td class='ui-widget-header '>Bun</td><td><input type='text' name='item_bun' id='bun_item' value='" . $rows['bun'] . "'></td></tr>";
-	echo "<tr><td class='ui-widget-header '>CC</td><td><input type='text' name='item_cc' id='cc_item' value='" . $rows['cc'] . "'></td></tr>";
+	echo "<tr><td class='ui-widget-header '>Bin</td><td><input type='text' maxlength='10' name='item_bin' id='bin_item' value='" . $rows['bin'] . "' class='letter number dash required'></td></tr>";
+	echo "<tr><td class='ui-widget-header '>Bundle</td><td><input type='text' maxlength='10' name='item_bun' id='bun_item' value='" . $rows['bun'] . "' class='letter required'></td></tr>";
+	echo "<tr><td class='ui-widget-header '>Cost Center</td><td><input type='text' maxlength='10' name='item_cc' id='cc_item' value='" . $rows['cc'] . "' class='letter'></td></tr>";
 	echo "<tr><td class='ui-widget-header '>Type</td><td>" . $type . "</td></tr>";
 	echo "<tr><td class='ui-widget-header '>Machine</td><td>
 			
@@ -66,6 +66,6 @@
 echo "</tbody>";
 echo "</table>";
 echo "<input type='hidden' name='item_edit1' id='edit1' value=" . $rows['matno'] . ">";
-echo "<div id='dialogSmaller'><input type='submit' value='Save' name='edit' onclick='editprocess();' class='ui-state-default ui-corner-all' ></div>";
+echo "<div id='dialogSmaller'><input type='submit' value='Save' name='edit' onclick='processEditItem();' class='ui-state-default ui-corner-all' ></div>";
 
 ?>
