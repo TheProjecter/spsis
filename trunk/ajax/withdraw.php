a4b2a6fb00bdd015cf93559c164bdabc0cec918c
rosemay.beltran <rosemay.beltran@4ce4fa8e-10c2-11df-8156-69f583257f85>
2010-03-22 16:04:55 +0000

    checking

--- trunk/ajax/withdraw.php
+++ trunk/ajax/withdraw.php
@@ -10,7 +10,7 @@
 <table id="usersDialogW" class="ui-widget ui-widget-content">
 	<tr><td colspan="2">
 		<input type='hidden' name='stock_w' id='id_stock_w' value="<?php print $rows['stock'] ?>"/>
-		ITEM: <a id="highlight"><b><?php print $rows['desc1'] ?></a></b></td></tr>
+		ITEM: <a id="highlight"><b><?php print $rows['matno'] ?></a></b></td></tr>
 	<tr><td>AMOUNT<b id="asterisk">*</b></td>
 		<td><input type='text' name='in_withdraw' id='id_withdraw' class="number required"><input type='hidden' name='item_w' id='id_item_w' value="<?php print $rows['matno']?>"></td>
 	</tr>
